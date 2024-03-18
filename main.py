1f21072e78ec50e7ff042fed753caa30edb568cf
lozert <90326151+lozert@users.noreply.github.com>
2024-03-19 02:07:11 +0500

    Update main.py
    Пока без успешно

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 # Import required libraries
-# import torch
-# import torchvision
-# from torchvision.io import read_image
-# import torchvision.transforms as T
+import torch
+import torchvision
+from torchvision.io import read_image
+import torchvision.transforms as T
 import numpy as np 
+import matplotlib as mpl
+import cv2
+from matplotlib import pyplot as plt
 
-gayss_matrix = np.array([[0.00000067, 0.00002292, 0.00019117, 0.00038771, 0.00019117, 0.00002292, 0.00000067],
+low_gayss_matrix = np.array([[0.00000067, 0.00002292, 0.00019117, 0.00038771, 0.00019117, 0.00002292, 0.00000067],
                         [0.00002292, 0.00078633, 0.00655965, 0.01330373, 0.00655965, 0.00078633, 0.00002292],
                         [0.00019117, 0.00655965, 0.05472157, 0.11098164, 0.05472157, 0.00655965, 0.00019117],
                         [0.00038771, 0.01330373, 0.11098164, 0.22508352, 0.11098164, 0.01330373, 0.00038771],
@@ -13,25 +16,60 @@ gayss_matrix = np.array([[0.00000067, 0.00002292, 0.00019117, 0.00038771, 0.0001
                         [0.00002292, 0.00078633, 0.00655965, 0.01330373, 0.00655965, 0.00078633, 0.00002292],
                         [0.00000067, 0.00002292, 0.00019117, 0.00038771, 0.00019117, 0.00002292, 0.00000067]])
 
-low_gayss_matrix = np.array([[0.4, 0.3, 0.2],
-                             [0.4, 0.3, 0.2],
-                             [0.4, 0.3, 0.2]])
+img = cv2.imread('snow.jpg')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+# low_gayss_matrix = np.array([[0.4, 0.3, 0.2],
+#                              [0.4, 0.3, 0.2],
+#                              [0.4, 0.3, 0.2]])
 
 
-img = [[1, 2, 3, 1, 1, 1, 1],
-       [2, 3, 1, 1, 1, 1, 1],
-       [1, 4, 5, 1, 1, 1, 1],
-       [1, 1, 1, 1, 1, 1, 1],
-       [1, 1, 1, 1, 1, 1, 1],
-       [1, 1, 1, 1, 1, 1, 1],
-       [1, 1, 1, 1, 1, 1, 1]]
+# img = np.array([[[1, 2, 3, 1, 1, 1, 1],
+#                 [2, 3, 1, 1, 1, 1, 1],
+#                 [1, 4, 5, 1, 1, 1, 1],
+#                 [1, 1, 1, 1, 1, 1, 1],
+#                 [1, 1, 1, 1, 1, 1, 1],
+#                 [1, 1, 1, 1, 1, 1, 1],
+#                 [1, 1, 1, 1, 1, 1, 1],
+#                 [1, 1, 1, 1, 1, 1, 1],
+#                 [1, 1, 1, 1, 1, 1, 1]],
+                
+#                 [[1, 2, 3, 1, 1, 1, 1],
+#                 [2, 3, 1, 1, 1, 1, 1],
+#                 [1, 4, 5, 1, 1, 1, 1],
+#                 [1, 1, 1, 1, 1, 1, 1],
+#                 [1, 1, 1, 1, 1, 1, 1],
+#                 [1, 1, 1, 1, 1, 1, 1],
+#                 [1, 1, 1, 1, 1, 1, 1],
+#                 [1, 1, 1, 1, 1, 1, 1],
+#                 [1, 1, 1, 1, 1, 1, 1]]])
+
+print(img.shape)
+
+color_img = img.shape[2]
+height_img = img.shape[0]
+width_img = img.shape[1]
+
+height_filter = low_gayss_matrix.shape[0]
+width_filter = low_gayss_matrix.shape[1]
+
+print(low_gayss_matrix.shape)
+print(color_img)
+print(height_img)
+print(width_img)
 
 
 
-new = np.zeros((len(img) + len(low_gayss_matrix) - 1, len(img[0]) + len(low_gayss_matrix[0]) - 1) )
-new[1: -int(len(low_gayss_matrix)/2), 1: -int(len(low_gayss_matrix[0])/2)] = img #Расширенная матрица на нули, чтоб матрицу можно было прогнать по всем элементам не сварачивая основное
+new = np.zeros((height_img + int((width_filter+1) /2) , width_img + int((width_filter+1)/2) , color_img))
+print(new.shape)
+for i in range(color_img):
+    new[ 1: -int(len(low_gayss_matrix)/2), 1: -int(len(low_gayss_matrix[0])/2), i] = img[:,:,i]
 
-y = np.zeros((len(img), len(img[0])))
+
+y = np.zeros((height_img, width_img, color_img))
+print(y.shape)
+count_color = 0
+
+
 
 def sumMatrix(small_matrix): # Свёртка 
     summ = 0
@@ -43,31 +81,30 @@ def sumMatrix(small_matrix): # Свёртка
     
     return summ
 
-for i in range(1, len(new) - 1):
-    for j in range(1, len(new) - 1):
-        
-        small_matrix = np.zeros((3,3))
-        
-        small_matrix = new[i-1:i+2,j-1:j+2]
-        # print(small_matrix)
-        
-        y[i-1][j-1] = sumMatrix(small_matrix)
-        
+while(count_color < color_img):
+    for i in range(int(height_filter / 2), new.shape[0] - int(height_filter / 2)):
+        for j in range(int(width_filter / 2), new.shape[1] - int(width_filter / 2)):
+            
+            small_matrix = np.zeros((height_filter, width_filter))
+          
+            small_matrix = new[i-int(height_filter / 2):i + int((height_filter+1) / 2),  j-int(width_filter/2):j+ int((width_filter+1)/2), count_color]
+            y[i- int(height_filter/2)][j- int(width_filter/2)][count_color] = sumMatrix(small_matrix)
+
+    count_color += 1
       
+
 print(y)
 
+plt.imshow(y)
+plt.show()
 
-step_left = len(img[0]) - len(gayss_matrix[0])
-step_down = len(img) - len(gayss_matrix)
-
-print(gayss_matrix.shape)
+# PIL_img = T.ToPILImage()(y)
 
 
-# read the jpg image
-# pic = read_image('nature.jpg')
-# print(pic.shape)
-# # convert this torch tensor to PIL image 
-# PIL_img = T.ToPILImage()(pic)
 
-# # display result
+# # print(pic.shape)
+# # # convert this torch tensor to PIL image 
+# # PIL_img = T.ToPILImage()(pic)
+
+# # # display result
 # PIL_img.show()
